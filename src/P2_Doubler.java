d0342429e0f6a4463db82d172eb54cfe3eabe8fa
quehlman51 <33034326+quehlman51@users.noreply.github.com>
2018-05-06 23:48:49 -0500

    Created input reader and doubler output

--- src/P2_Doubler.java
+++ src/P2_Doubler.java
@@ -1,13 +1,18 @@
+import java.util.Scanner;
+
 /*
 	ISYS 320
-	Name(s):
+	Name(s): Anthony Quehl
 	Date: 
 */
 
 public class P2_Doubler {
 
 	public static void main(String[] args) {
-
+		Scanner console = new Scanner(System.in);
+		System.out.print("What is your number? ");
+		int number = console.nextInt();
+		System.out.print("Your number doubled is: " + (number * 2));
 	}
 
 }
