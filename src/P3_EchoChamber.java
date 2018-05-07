20bcef93a9906a3b57275fb59d3aa0518214c46f
quehlman51 <33034326+quehlman51@users.noreply.github.com>
2018-05-06 23:40:11 -0500

    Created method

--- src/P3_EchoChamber.java
+++ src/P3_EchoChamber.java
@@ -1,13 +1,23 @@
+import java.util.Scanner;
+
 /*
 	ISYS 320
-	Name(s):
+	Name(s): Anthony Quehl
 	Date: 
 */
 
 public class P3_EchoChamber {
 
 	public static void main(String[] args) {
-
+		Scanner console = new Scanner(System.in);
+		System.out.print("What is your phrase? ");		 
+		String phrase = console.nextLine(); 
+		System.out.print("How many times to repeat? ");
+		int number = console.nextInt(); 
+		for (int i = 1; i <= number; i++)
+		{
+			System.out.println(phrase);
+		}
 	}
 
 }
